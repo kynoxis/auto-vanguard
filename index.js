7b1147eb82ec55f584f7a3136f5acfb518b53973
Kyon <kynoxis@mail.com>
2018-01-16 18:30:32 -0200

    Auto update

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+
+const Command = require('command');
+
 module.exports = function AutoVanguard(dispatch) {
-  dispatch.hook('sCompleteEventMatchingQuest', function(event) {
-    dispatch.toServer('cCompleteDailyEvent', { id: event.id });
-    return false;
-  });
+	const command = Command(dispatch);
+	
+	let enabled = true;
+	
+	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, (event) => {
+		if (!enabled)
+			return false;
+			
+		dispatch.toServer('C_COMPLETE_DAILY_EVENT', 1, { id: event.id });
+		
+		//tries to complete daily and weekly rewards
+		dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 });
+		dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 });
+		return false;
+	});
+	
+	command.add('autovg', () => {
+		enabled = !enabled
+		command.message('[Auto Vanguard] ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
+	})
 };
